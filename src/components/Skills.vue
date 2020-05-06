4952b9020da6c2c048aa6e4c6d11005d1d8879e0
Josh Riches <jriches@Joshs-MacBook-Pro.local>
2020-05-05 21:29:53 -0600

    learning how to access data and set multiple classes

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,32 +1,33 @@
 <template>
-  <div class="hello">
-    <h1>Skills</h1>
-  </div>
+    <div class="holder">
+      <ul class="list">
+        <li v-for="(data, index) in skills" :key='index'>{{ index }}. {{ data.skill }}</li>
+      </ul>
+
+      <div v-bind:class="alertObject"></div>
+    </div>
 </template>
 
 <script>
 export default {
   name: 'Skills',
-  props: {
-    msg: String
+  data() {
+    return {
+      skills: [
+        { "skill": "Vue.js"},
+        { "skill": "React.js"},
+        { "skill": "FrontEnd"},
+        { "skill": "FrontEnd"}
+      ],
+      alertObject: {
+        alert: true,
+        anotherClass: true,
+      }
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style src="../css/Skills.css" scoped>
 </style>
